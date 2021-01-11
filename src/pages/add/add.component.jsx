e6234a8ee9a8e40a1182c33b062cf916f45c3737
mitej23 <you@example.com>
2021-01-12 00:32:27 +0530

    form validation added

--- src/pages/add/add.component.jsx
+++ src/pages/add/add.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Formik, Field, Form } from "formik";
 import JSONDATA from "../../data.json";
 import Select from "react-select";
 import "./add.styles.css";
@@ -8,68 +9,83 @@ const options = JSONDATA;
 class AddFund extends React.Component {
   state = {
     selectedOptions: null,
+    amount: 0,
+    date: undefined,
   };
-  handleChange = (selectedOption) => {
-    this.setState({ selectedOption });
-    console.log(`Option selected:`, selectedOption);
+  handleChange = (event) => {
+    this.setState({ selectedOptions: event });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
   };
   render() {
     const { selectedOption } = this.state;
+    // const { amount } = this.state;
+    // const { date } = this.state;
     return (
       <div>
         <div id="outer-div">
-          <p id="title">Add Fund</p>
-          <div id="fund">
-            <p id="fund-title">Select a Fund</p>
+          <h1 id="title">Add Fund</h1>
+          <h2 id="fund-title">Select a Fund</h2>
+          <Formik
+            initialValues={{ amount: 0, date: null }}
+            onSubmit={async (values) => {
+              await new Promise((resolve) => setTimeout(resolve, 500));
+              //alert(JSON.stringify(values, null, 2));
+              this.setState(values);
+              console.log(this.state);
+            }}
+          >
+            <Form>
+              <Select
+                id="selectFund"
+                value={selectedOption}
+                onChange={this.handleChange}
+                options={options}
+              />
+              <h2 id="amt-title">Amount Invested</h2>
+              <Field name="amount" type="number" id="amt-input" />
+              <br />
+              <h2 id="date-title">Date of Investement</h2>
+              <Field name="date" type="date" id="date-input" />
+              <br />
+              <button type="submit" id="submit">
+                Submit
+              </button>
+            </Form>
+          </Formik>
+          {/* <form onSubmit={this.handleSubmit}>
+            <h1 id="title">Add Fund</h1>
+            <h2 id="fund-title">Select a Fund</h2>
             <Select
-              class="selectFund"
-              value={selectedOption}
+              id="selectFund"
+              // value={selectedOption}
               onChange={this.handleChange}
               options={options}
             />
-          </div>
-          <div id="amt-invested">
-              <p id="amt-title">Amount Invested</p>
-              <input placeholder="..." id="amt-input" />
-          </div>
+            <h2 id="amt-title">Amount Invested</h2>
+            <input
+              id="amt-input"
+              type="number"
+              name="amount"
+              // value={amount}
+              onChange={this.handleChange}
+            />
+            <h2 id="date-title">Date of Investement</h2>
+            <input
+              type="date"
+              id="date-input"
+              name="date"
+              // value={date}
+              onChange={this.handleChange}
+            />
+            <br />
+            <input type="submit" value="Add" id="submit" />
+          </form> */}
         </div>
       </div>
     );
   }
 }
 
-// function AddFund() {
-//   const [searchTerm, setSearchTerm] = useState("");
-
-//   return (
-//     <div>
-//       <input
-//         placeholder="Type Fund name.."
-//         type="text"
-//         onChange={(event) => {
-//           setSearchTerm(event.target.value);
-//           console.log(searchTerm);
-//         }}
-//       />
-//       {JSONDATA.filter((val) => {
-//         if (searchTerm.length < 7) {
-//           return "";
-//         } else if (
-//           val.field1.toLowerCase().includes(searchTerm.toLowerCase())
-//         ) {
-//           return val;
-//         }
-//       }).map((val, key) => {
-//         console.log(val.field1);
-//         return (
-//           <div className="user" key={key}>
-//             <p>
-//               {val.field1} its id ={val.field2}
-//             </p>
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// }
 export default AddFund;
