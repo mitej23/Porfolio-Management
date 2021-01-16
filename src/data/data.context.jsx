a40d844d5ad67470426f53c63a8a0f2a82f6791c
mitej23 <mitejmadan@gmail.com>
2021-01-16 16:49:09 +0530

    Removed initial data

--- src/data/data.context.jsx
+++ src/data/data.context.jsx
@@ -3,72 +3,7 @@ import React, { createContext, useState } from "react";
 export const FundContext = createContext();
 
 export const FundProvider = (props) => {
-  const [funds, addFund] = useState([
-    {
-      id: 1,
-      fundName: "ICICI Prudential Balanced Advantage Fund",
-      dateOfInvestment: new Date("2018, 1, 25"),
-      amtInvested: 25000,
-      code: 104686,
-    },
-    {
-      id: 2,
-      fundName: "HDFC Prudence Fund - Dividend Option",
-      dateOfInvestment: new Date("2018, 1, 24"),
-      amtInvested: 24900,
-      code: 101978,
-    },
-    {
-      id: 3,
-      fundName: "HDFC Prudence Fund - Dividend Option",
-      dateOfInvestment: new Date(2018, 2, 22),
-      amtInvested: 24900,
-      code: 101978,
-    },
-    {
-      id: 4,
-      fundName: "HDFC Prudence Fund - Dividend Option",
-      dateOfInvestment: new Date(2018, 4, 11),
-      amtInvested: 24900,
-      code: 101978,
-    },
-    {
-      id: 5,
-      fundName: "HDFC Prudence Fund - Dividend Option",
-      dateOfInvestment: new Date(2018, 4, 26),
-      amtInvested: 39900,
-      code: 101978,
-    },
-
-    {
-      id: 6,
-      fundName: "UTI MUTUAL FUND 2019",
-      dateOfInvestment: new Date(2019, 11, 14),
-      amtInvested: 20000,
-      code: 100033,
-    },
-    {
-      id: 7,
-      fundName: "LIC MUTUAL FUND 2019",
-      dateOfInvestment: new Date(2019, 5, 14),
-      amtInvested: 40000,
-      code: 100034,
-    },
-    {
-      id: 8,
-      fundName: "ICICI Prudential Balanced Advantage Fund",
-      dateOfInvestment: new Date("2018, 1, 25"),
-      amtInvested: 25000,
-      code: 104686,
-    },
-    {
-      id: 9,
-      fundName: "HDFC Prudence Fund - Dividend Option",
-      dateOfInvestment: new Date("2018, 1, 24"),
-      amtInvested: 24900,
-      code: 101978,
-    },
-  ]);
+  const [funds, addFund] = useState([]);
   return (
     <FundContext.Provider value={[funds, addFund]}>
       {props.children}
